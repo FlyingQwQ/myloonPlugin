787433769d19a1468c2faace0f06c2e1ac3df8dd
kuoer <flyingqwq@qq.com>
2026-01-20 15:53:21 +0800

    调整

--- oa.js
+++ oa.js
@@ -1,14 +1,2 @@
-// amap_detect.js
-
-// 只作为监听器，不修改请求
-
 console.log("🔥 AMap Script Triggered");
-
-// // 示例：只在 9 点前执行逻辑
-// if (now.getHours() < 9) {
-//   // 这里写你真正想做的事情
-//   console.log("Before 9 AM, execute logic");
-// }
-
-// 一定要放行
 $done({});
