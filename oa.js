433520884e8f22868ef4616b120e63687b32e1c1
kuoer <flyingqwq@qq.com>
2026-01-20 16:16:01 +0800

    调整

--- oa.js
+++ oa.js
@@ -1,3 +1,28 @@
+console.log("====== AMap http-request Triggered ======");
+
+// URL
+console.log("URL:", $request.url);
+
+// Method
+console.log("Method:", $request.method);
+
+// Headers
+console.log("Headers:", JSON.stringify($request.headers, null, 2));
+
+// Body（如果存在）
+if ($request.body) {
+  console.log("Body:", $request.body);
+} else {
+  console.log("Body: <EMPTY>");
+}
+
+console.log("=========================================");
+
+$done({});
+
+
+
+
 // 读取请求 body
 let requestBody = $request.body || ""; // POST 请求一般在 body 中
 let requestUrl = $request.url || "";   // GET 请求可能在 URL 查询参数中
