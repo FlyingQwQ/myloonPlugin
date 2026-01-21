f958a7e0327d2cf100331bce384fbd4a235e8c45
kuoer <flyingqwq@qq.com>
2026-01-21 13:56:53 +0800

    调整

--- oa.js
+++ oa.js
@@ -1,17 +1,37 @@
-const requestUrl = $request.url || "";
-console.log("URL:", requestUrl);
+// const requestUrl = $request.url || "";
+// console.log("URL:", requestUrl);
 
 // Method
-console.log("Method:", $request.method);
+// console.log("Method:", $request.method);
 
 // Headers
-console.log("Headers:", JSON.stringify($request.headers, null, 2));
+// console.log("Headers:", JSON.stringify($request.headers, null, 2));
+
+
+const TIME_KEY = "oa_last_exec_time";
+
+// 读取上一次
+const lastTime = $peristentStore.read(TIME_KEY);
+
+if (lastTime) {
+  console.log("上一次执行时间:", new Date(Number(lastTime)).toLocaleString());
+} else {
+  console.log("第一次执行，尚无历史时间");
+}
+
+// 覆盖写入当前时间
+const now = Date.now();
+$peristentStore.write(String(now), TIME_KEY);
+
+console.log("本次执行时间:", new Date(now).toLocaleString());
+
+
 
 
 $notification.post(
   "检测到签到页面",
-  "触发",
-  "请求 URL 或 Body 中包含关键字"
+  "",
+  "当前时间: " + new Date(now).toLocaleString(),
 );
 
 // 一定要放行请求
